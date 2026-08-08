01ce2ed17aba4be49845ca1cc591647902596019
zfan0088 <zfan0088@student.monash.edu>
2026-08-08 20:45:46 +0800

    Complete lab 12

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/Lab10/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
